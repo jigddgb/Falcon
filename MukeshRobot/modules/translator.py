7b76489c4ef2a7625daf86cdb49b7f9abd8e914b
Rik <wriddhammandal1307@gmail.com>
2024-02-04 13:24:12 +0530

    Update translator.py

--- MukeshRobot/modules/translator.py
+++ MukeshRobot/modules/translator.py
@@ -78,7 +78,7 @@ from telethon import __version__ as s
 from MukeshRobot import OWNER_ID, dispatcher
 from MukeshRobot import pbot as client
 
-Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
+Mukesh = "https://graph.org/file/8f2a96ad80cf77d92f7f2.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
@@ -93,21 +93,7 @@ async def repo(client, message):
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ɢʀᴏᴜᴘ ✘ ᴄᴏɴᴛʀᴏʟʟᴇʀ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
-""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "• ᴏᴡɴᴇʀ ",user_id=OWNER_ID
-                    ),
-                    InlineKeyboardButton(
-                        "• ʀᴇᴘᴏ •",
-                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x4E\x6F\x6F\x62\x2D\x4D\x75\x6B\x65\x73\x68\x2F\x4D\x75\x6B\x65\x73\x68\x52\x6F\x62\x6F\x74",
-                    ),
-                ]
-            ]
-        ),
+"""
     )
 
 
