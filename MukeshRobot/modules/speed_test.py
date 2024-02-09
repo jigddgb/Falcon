fff1888f941f7031e0c23776b9435fc3c7437389
Rik <wriddhammandal1307@gmail.com>
2024-02-09 15:45:02 +0530

    Update speed_test.py

--- MukeshRobot/modules/speed_test.py
+++ MukeshRobot/modules/speed_test.py
@@ -47,7 +47,7 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
             replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
             update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.answer("You are required to join @the_support_chat to use this command.")
+        query.answer("You are required to join @TheFalconSupport to use this command.")
 
 
 SPEED_TEST_HANDLER = DisableAbleCommandHandler(
